623679503de1badf523f55b7d84a680cabb7e42a
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2018-11-04 08:44:51 +0900

    Tenka1 Programmer Beginner Contest C 解説を見て実装

--- src/Tenka1ProgrammerBeginnerContest/C.hs
+++ src/Tenka1ProgrammerBeginnerContest/C.hs
@@ -1,20 +1,31 @@
 module Tenka1ProgrammerBeginnerContest.C where
 
+import           Control.Arrow ((&&&))
 import           Control.Monad (replicateM)
-import           Data.List     (permutations)
+import           Data.Function (on)
+import           Data.List     (sort)
+import           Data.Tuple    (swap)
 
 main :: IO ()
 main = do
   n <- readLn
   as <- replicateM n readLn
-  print $ f as
+  print $ f n as
 
-f :: [Int] -> Int
--- C012_scrambled 以降 TLE
-f = maximum . map (sum . (zipWith ((abs .) . subtract) <*> tail)) . permutations
+f :: Int -> [Int] -> Int
+f n as = max (sum' cs1) (sum' cs2)
+  where
+    (cs1, cs2) = coefficients n
+    sum' = sum . (zipWith (*) `on` sort) as
+
+coefficients :: Int -> ([Int], [Int])
+coefficients n | n < 2 = undefined
+coefficients n = unzip $ zipWith ($) (cycle [id, swap]) $ map (id &&& negate) ns
+  where
+    ns = [1] ++ replicate (n - 2) 2 ++ [1]
 
 tests, test1, test2, test3 :: Bool
 tests = test1 && test2 && test3
-test1 = f [6, 8, 1, 2, 3] == 21
-test2 = f [3, 1, 4, 1, 5, 9] == 25
-test3 = f [5, 5, 1] == 8
+test1 = f 5 [6, 8, 1, 2, 3] == 21
+test2 = f 6 [3, 1, 4, 1, 5, 9] == 25
+test3 = f 3 [5, 5, 1] == 8
