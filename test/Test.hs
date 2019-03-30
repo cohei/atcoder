b3fd22b355246cf426f89c14c37037a187613380
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2019-03-31 00:40:43 +0900

    main-tester で例外が起きたときはそれを投げる
    標準出力だけ見ていると、何の例外が起きたかわからない

--- test/Test.hs
+++ test/Test.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Test (shouldInteractAs) where
 
-import           Data.ByteString (ByteString)
-import           Test.Hspec      (shouldBe)
-import           Test.Main       (captureProcessResult, prStdout, withStdin)
+import           Control.Exception (throwIO)
+import           Data.ByteString   (ByteString)
+import           Test.Hspec        (shouldBe)
+import           Test.Main         (ProcessResult (ProcessResult, prException, prStdout),
+                                    captureProcessResult, withStdin)
 
 shouldInteractAs :: IO () -> (ByteString, ByteString) -> IO ()
 shouldInteractAs action (input, output) = do
-  result <- captureProcessResult $ withStdin input action
-  prStdout result `shouldBe` output
+  ProcessResult { prException, prStdout } <- captureProcessResult $ withStdin input action
+  maybe (pure ()) throwIO prException
+  prStdout `shouldBe` output
