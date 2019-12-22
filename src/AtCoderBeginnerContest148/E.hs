778c9227424eb9321fa0edfd0f131dea4eab872d
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2019-12-22 23:23:46 +0900

    AtCoder Beginner Contest 148 E 解説を見て実装

--- src/AtCoderBeginnerContest148/E.hs
+++ src/AtCoderBeginnerContest148/E.hs
@@ -8,8 +8,4 @@ main = do
 solve :: Int -> Int
 solve n
   | odd n = 0
-  -- | otherwise = sum (takeWhile (/= 0) $ tail $ iterate (`div` 10) n) + (n `div` 10) `div` 5 - (n `div` 10) `div` 10
-  | otherwise = sum $ map (\m -> m + m `div` 5 - m `div` 10) $ tail $ takeWhile (/= 0) $ iterate (`div` 10) n
-
-f :: Integer -> Integer
-f n = if n < 2 then 1 else  n * f (n - 2)
+  | otherwise = sum $ takeWhile (/= 0) $ iterate (`div` 5) $ n `div` 10
