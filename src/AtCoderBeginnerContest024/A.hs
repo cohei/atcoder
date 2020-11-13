7f74ee8421ac1eeab4a474d88db77d7d073a39fe
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2020-11-13 15:08:57 +0900

    AtCoder Beginner Contest 024 A 警告を直す

--- src/AtCoderBeginnerContest024/A.hs
+++ src/AtCoderBeginnerContest024/A.hs
@@ -1,22 +1,21 @@
 module AtCoderBeginnerContest024.A (main) where
 
 main :: IO ()
-main = interact $ (++ "\n") . show . total' . readInput
-  where
-    total' :: (Num a, Ord a) => (a, a, a, a, a, a) -> a
-    total' (childPrice, adultPrice, discount, group, childCount, adultCount) =
-      total childPrice adultPrice discount group childCount adultCount
+main = interact $ (++ "\n") . maybe "" (show . uncurry6 total) . readInput
+
+uncurry6 :: (a -> b -> c -> d -> e -> f -> g) -> (a, b, c, d, e, f) -> g
+uncurry6 function (a, b, c, d, e, f) = function a b c d e f
 
 type Price = Int
 type Count = Int
 
-readInput :: String -> (Price, Price, Price, Count, Count, Count)
-readInput s =
-  let
-    [firstLine, secondLine] = take 2 $ lines s
-    [childPrice, adultPrice, discount, group] = map read $ words firstLine
-    [childCount, adultCount] = map read $ words secondLine
-  in (childPrice, adultPrice, discount, group, childCount, adultCount)
+readInput :: String -> Maybe (Price, Price, Price, Count, Count, Count)
+readInput s
+  | [firstLine, secondLine]                   <- take 2 $ lines s
+  , [childPrice, adultPrice, discount, group] <- map read $ words firstLine
+  , [childCount, adultCount]                  <- map read $ words secondLine =
+      Just (childPrice, adultPrice, discount, group, childCount, adultCount)
+  | otherwise = Nothing
 
 total :: (Num a, Ord a) => a -> a -> a -> a -> a -> a -> a
 total childPrice adultPrice discount group childCount adultCount =
